068319c6bdaaa562e2b104c68561bcf60b2d6ada
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2022-09-20 00:43:25 +0530

    skeleton

---
+++ src/Components/Merch.js
@@ -0,0 +1,186 @@
+import React from "react";
+
+const Merch = () => {
+  return (
+    <>
+      <section>
+        <div class="max-w-screen-xl px-4 py-8 mx-auto">
+          <div>
+            <span class="inline-block w-12 h-1 bg-red-700"></span>
+
+            <h2 class="mt-1 text-2xl font-extrabold tracking-wide uppercase lg:text-3xl">
+              Trainers
+            </h2>
+          </div>
+
+          <div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
+            <a href="#" class="block">
+              <div class="flex justify-center">
+                <strong class="relative h-6 px-4 text-xs leading-6 text-white uppercase bg-black">
+                  New
+                </strong>
+              </div>
+
+              <img
+                alt="Trainer"
+                src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                class="object-cover w-full -mt-3 h-96"
+              />
+
+              <h5 class="mt-4 text-sm text-black/90">
+                Limited Edition Sports Trainer
+              </h5>
+
+              <div class="flex items-center justify-between mt-4 font-bold">
+                <p class="text-lg">$189.99</p>
+
+                <p class="text-xs tracking-wide uppercase">6 Colors</p>
+              </div>
+            </a>
+
+            <a href="#" class="block">
+              <div class="flex justify-center">
+                <strong class="relative h-6 px-4 text-xs leading-6 text-white uppercase bg-black">
+                  New
+                </strong>
+              </div>
+
+              <img
+                alt="Trainer"
+                src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                class="object-cover w-full -mt-3 h-96"
+              />
+
+              <h5 class="mt-4 text-sm text-black/90">
+                Limited Edition Sports Trainer
+              </h5>
+
+              <div class="flex items-center justify-between mt-4 font-bold">
+                <p class="text-lg">$189.99</p>
+
+                <p class="text-xs tracking-wide uppercase">6 Colors</p>
+              </div>
+            </a>
+
+            <a href="#" class="block">
+              <div class="flex justify-center">
+                <strong class="relative h-6 px-4 text-xs leading-6 text-white uppercase bg-black">
+                  New
+                </strong>
+              </div>
+
+              <img
+                alt="Trainer"
+                src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                class="object-cover w-full -mt-3 h-96"
+              />
+
+              <h5 class="mt-4 text-sm text-black/90">
+                Limited Edition Sports Trainer
+              </h5>
+
+              <div class="flex items-center justify-between mt-4 font-bold">
+                <p class="text-lg">$189.99</p>
+
+                <p class="text-xs tracking-wide uppercase">6 Colors</p>
+              </div>
+            </a>
+
+            <a href="#" class="block">
+              <div class="flex justify-center">
+                <strong class="relative h-6 px-4 text-xs leading-6 text-white uppercase bg-black">
+                  New
+                </strong>
+              </div>
+
+              <img
+                alt="Trainer"
+                src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                class="object-cover w-full -mt-3 h-96"
+              />
+
+              <h5 class="mt-4 text-sm text-black/90">
+                Limited Edition Sports Trainer
+              </h5>
+
+              <div class="flex items-center justify-between mt-4 font-bold">
+                <p class="text-lg">$189.99</p>
+
+                <p class="text-xs tracking-wide uppercase">6 Colors</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
+          <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:items-stretch">
+            <div class="flex items-center p-8 bg-gray-100 rounded">
+              <div class="mx-auto text-center lg:text-left">
+                <h2 class="text-2xl font-bold">Watches</h2>
+
+                <p class="mt-4 text-sm text-gray-700 max-w-[45ch]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+                  cupiditate mollitia saepe vitae libero nobis.
+                </p>
+
+                <a
+                  href="#"
+                  class="inline-block px-6 py-3 mt-6 text-sm text-white bg-black rounded"
+                >
+                  View the Range
+                </a>
+              </div>
+            </div>
+
+            <div class="grid grid-cols-2 gap-4 lg:col-span-2 lg:grid-cols-3 lg:py-12">
+              <a href="#" class="block">
+                <img
+                  alt="Simple Watch"
+                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
+                  class="object-cover w-full rounded aspect-square"
+                />
+
+                <div class="mt-2">
+                  <h5 class="font-medium">Simple Watch</h5>
+
+                  <p class="mt-1 text-sm text-gray-700">$150</p>
+                </div>
+              </a>
+
+              <a href="#" class="block">
+                <img
+                  alt="Simple Watch"
+                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
+                  class="object-cover w-full rounded aspect-square"
+                />
+
+                <div class="mt-2">
+                  <h5 class="font-medium">Simple Watch</h5>
+
+                  <p class="mt-1 text-sm text-gray-700">$150</p>
+                </div>
+              </a>
+
+              <a href="#" class="block">
+                <img
+                  alt="Simple Watch"
+                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
+                  class="object-cover w-full rounded aspect-square"
+                />
+
+                <div class="mt-2">
+                  <h5 class="font-medium">Simple Watch</h5>
+
+                  <p class="mt-1 text-sm text-gray-700">$150</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Merch;
